e4fa3975fa73e66f06f6ed5fe53534db130b80b6
+ <+>
2022-07-20 17:16:40 +0100

    commit tree dfde1e31e82504078ebbbce51be6116bd44e4525

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+
+
+const gifts = ['teddy bear','drone', 'doll'];
+
+function wrapGifts(gifts){
+    for (let i=0; i<gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+*/
+
+function writeCards(names,event){
+    const thankYouMessages = []
+    for (let i=0; i < names.length; i++){
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thankYouMessages;
+}
+
+function countDown(){
+    let i = 10;
+    while (i>=0){
+        console.log(i--);
+    }
+}
